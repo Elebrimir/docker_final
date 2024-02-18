6a3b2b9fefdefac59abcb51c2e6f45ff789b61b5
Pablo Cortés <cortesoltra@gmail.com>
2024-02-18 17:04:07 +0100

    ja funciona

--- mongo/mongo-init.sh
+++ mongo/mongo-init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Restaurar la base de datos desde el archivo de volcado
-mongorestore -d movies /docker-entrypoint-initdb.d
+#mongorestore -d movies /docker-entrypoint-initdb.d
+mongorestore -d  movies ./docker-entrypoint-initdb.d/movies
